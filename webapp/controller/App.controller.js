e45a038b7560b48aea90b624606f0a330454008b
Jeswin Noelraj <jeswin.noelraj@mobolutions.com>
2020-09-22 11:00:08 +0000

    test

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,7 @@ sap.ui.define([
 	return BaseController.extend("zee.POGrunt.controller.App", {
 
 		onInit : function () {
+			/*tests*/
 			var oViewModel,
 				fnSetAppNotBusy,
 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
